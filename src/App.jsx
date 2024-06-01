a727e451bc7c400650c37b919788ed9a620c99fc
kwakjaehun <jh5414092@icloud.com>
2024-06-01 14:39:48 +0900

    Type : Feat
    Add a Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { router } from '@/routes';
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import { supabase } from './supabase';
 
 const Globalstyle = createGlobalStyle`
   ${reset}
 `;
 
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+      const data = await supabase.from('page').select('*');
+      console.log(data);
+    };
+    getData();
+  });
+
   return (
     <>
       <Globalstyle />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
+import { Card } from '@/components/Card';
 import { StyledHome } from './StyledHome';
 
 const Home = () => {
-  return <StyledHome>Hello My Team!</StyledHome>;
+  return (
+    <>
+      <StyledHome>Hello My Team!</StyledHome>
+      <Card />
+    </>
+  );
 };
 
 export { Home };
