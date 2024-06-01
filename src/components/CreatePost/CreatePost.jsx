ebd3473de0f3f66ee04efca8aca77619297f62b9
Minsu <alstn549@naver.com>
2024-06-01 14:48:02 +0900

    create branch

---
+++ src/components/CreatePost/CreatePost.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const sample = {
+  hashtags: ["Front-end", "Back-end", "Designer", "UI Designer"],
+  title: "Hello World!",
+  body: "Hello My Team!!! Occaecat dolore ex minim duis do exercitation aute excepteur incididunt do dolore ad. Occaecat dolore ex minim duis do exercitation aute excepteur incididunt do dolore ad. Occaecat dolore ex minim duis do exercitation aute excepteur incididunt do dolore ad.",
+  name: "James",
+  job: "Web Developer",
+  likesCount: 126,
+  commentsCount: 12,
+};
+
+const CreatePost = () => {
+  return <div>CreatePost</div>;
+};
+
+export default CreatePost;
