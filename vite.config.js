562aa46cd948ca6c430cb4948eb0d54ac8d0aaed
kwakjaehun <jh5414092@icloud.com>
2024-06-04 10:40:25 +0900

    add alias

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@components', replacement: path.resolve(__dirname, '/src/components') },
+      { find: '@utils', replacement: path.resolve(__dirname, '/src/utils') },
+      { find: '@hooks', replacement: path.resolve(__dirname, '/src/hooks') },
+      { find: '@pages', replacement: path.resolve(__dirname, '/src/pages') },
+      { find: '@redux', replacement: path.resolve(__dirname, '/src/redux') }
+    ]
+  }
 });
