d681592093f103c5b27d067ec1ee3a019602fe01
Minsu <alstn549@naver.com>
2024-06-04 10:52:37 +0900

    11

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
